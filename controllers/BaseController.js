5c11ddd580655e25e9922ec0ca2b5cb298fa568b
vasu.senawongse <vasu@coursesquare.co>
2021-07-11 16:57:17 +0700

    init

---
+++ controllers/BaseController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index(req, res) {
+    res.send('LOEI-A6 API V 1.0.0');
+  },
+};
---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+const BaseController = require('./../controllers/BaseController');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, req.body.path);
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer({ storage });
+module.exports = (app) => {
+  app.use(function (req, res, next) {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
+    );
+    next();
+  });
+  app.get('/', BaseController.index);
+  app.post('/upload', upload.single('file'), (req, res) => {
+    try {
+      res.send(req.file);
+    } catch (err) {
+      res.send(400);
+    }
+  });
+};
