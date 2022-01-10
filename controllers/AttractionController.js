b8b80e8e6ea0a5cbd46813e48ee5763660076523
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-10 18:39:49 +0700

    Update AttractionController.js

--- controllers/AttractionController.js
+++ controllers/AttractionController.js
@@ -159,7 +159,7 @@ module.exports = {
           var array = attractions[i].category.split(',')
           const index = array.indexOf(type[0].name)
           array[index] = payload.name
-          var category = array.join()
+          var category = array.filter(i => i != '').join()
           await sql.query(
             "UPDATE attractions SET category = ? WHERE id = ?",
             [
@@ -189,7 +189,7 @@ module.exports = {
           var array = attractions[i].amenities.split(',')
           const index = array.indexOf(amen[0].name)
           array[index] = payload.name
-          var amenity = array.join()
+          var amenity = array.filter(i => i != '').join()
           await sql.query(
             "UPDATE attractions SET amenities = ? WHERE id = ?",
             [
@@ -221,7 +221,7 @@ module.exports = {
           var array = attractions[i].amenitiesForAll.split(',')
           const index = array.indexOf(amen[0].name)
           array[index] = payload.name
-          var amenity = array.join()
+          var amenity = array.filter(i => i != '').join()
           await sql.query(
             "UPDATE attractions SET amenitiesForAll = ? WHERE id = ?",
             [
@@ -253,7 +253,7 @@ module.exports = {
           var array = attractions[i].activities.split(',')
           const index = array.indexOf(act[0].name)
           array[index] = payload.name
-          var activity = array.join()
+          var activity = array.filter(i => i != '').join()
           await sql.query(
             "UPDATE attractions SET activities = ? WHERE id = ?",
             [
