7b85332837ed4fe8f78421dfb973eaee1ce422ac
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-10 18:06:37 +0700

    Update AttractionController.js

--- controllers/AttractionController.js
+++ controllers/AttractionController.js
@@ -187,7 +187,7 @@ module.exports = {
 
         for (var i = 0; i < attractions.length; i++) {
           var array = attractions[i].amenities.split(',')
-          const index = array.indexOf(type[0].name)
+          const index = array.indexOf(amen[0].name)
           array[index] = payload.name
           var amenity = arr.join()
           await sql.query(
@@ -204,7 +204,41 @@ module.exports = {
           [payload.name, payload.id]
         );
         res.json(result);
-      } else if (payload.type == "ACT") {
+      }
+      else if (payload.type == "AMENFA") {
+
+        const amen = await sql.query(
+          'SELECT * FROM amenities WHERE id = ?',
+          [payload.id]
+        );
+
+        const attractions = await sql.query(
+          'SELECT * FROM attractions WHERE amenitiesForAll LIKE "%"?"%"',
+          [amen[0].name]
+        );
+
+        for (var i = 0; i < attractions.length; i++) {
+          var array = attractions[i].amenitiesForAll.split(',')
+          const index = array.indexOf(amen[0].name)
+          array[index] = payload.name
+          var amenity = arr.join()
+          await sql.query(
+            "UPDATE attractions SET amenitiesForAll = ? WHERE id = ?",
+            [
+              amenity,
+              attractions[i].id,
+            ]
+          );
+        }
+
+        result = await sql.query(
+          "UPDATE amenities SET name = ? WHERE id = ?",
+          [payload.name, payload.id]
+        );
+        res.json(result);
+      }
+
+      else if (payload.type == "ACT") {
         const act = await sql.query(
           'SELECT * FROM activities WHERE id = ?',
           [payload.id]
@@ -217,7 +251,7 @@ module.exports = {
 
         for (var i = 0; i < attractions.length; i++) {
           var array = attractions[i].activities.split(',')
-          const index = array.indexOf(type[0].name)
+          const index = array.indexOf(act[0].name)
           array[index] = payload.name
           var activity = arr.join()
           await sql.query(
@@ -286,11 +320,42 @@ module.exports = {
 
         for (var i = 0; i < attractions.length; i++) {
           var array = attractions[i].amenities.split(',')
-          const index = array.indexOf(type[0].name)
+          const index = array.indexOf(amen[0].name)
           var arr = array.splice(index, 1);
           var amenity = arr.join()
           await sql.query(
             "UPDATE attractions SET amenities = ? WHERE id = ?",
+            [
+              amenity,
+              attractions[i].id,
+            ]
+          );
+        }
+
+        result = await sql.query(
+          "DELETE FROM amenities WHERE id = ?",
+          [payload.id]
+        );
+        res.json(result);
+      } else if (payload.type == "AMENFA") {
+
+        const amen = await sql.query(
+          'SELECT * FROM amenities WHERE id = ?',
+          [payload.id]
+        );
+
+        const attractions = await sql.query(
+          'SELECT * FROM attractions WHERE amenitiesForAll LIKE "%"?"%"',
+          [amen[0].name]
+        );
+
+        for (var i = 0; i < attractions.length; i++) {
+          var array = attractions[i].amenitiesForAll.split(',')
+          const index = array.indexOf(amen[0].name)
+          var arr = array.splice(index, 1);
+          var amenity = arr.join()
+          await sql.query(
+            "UPDATE attractions SET amenitiesForAll = ? WHERE id = ?",
             [
               amenity,
               attractions[i].id,
@@ -316,7 +381,7 @@ module.exports = {
 
         for (var i = 0; i < attractions.length; i++) {
           var array = attractions[i].activities.split(',')
-          const index = array.indexOf(type[0].name)
+          const index = array.indexOf(act[0].name)
           var arr = array.splice(index, 1);
           var activity = arr.join()
           await sql.query(
